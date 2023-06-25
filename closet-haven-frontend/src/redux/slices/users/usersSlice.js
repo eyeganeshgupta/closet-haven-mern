0605cff86a7ffd7f194b8bb31afbca23d996f12b
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-06-25 13:47:49 +0530

    login-action--and--users-slices

--- closet-haven-frontend/src/redux/slices/users/usersSlice.js
+++ closet-haven-frontend/src/redux/slices/users/usersSlice.js
@@ -1,3 +1,7 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import baseURL from "../../../utils/baseURL";
+
 // initialState
 const initialState = {
   loading: false,
@@ -11,3 +15,50 @@ const initialState = {
     userInfo: {},
   },
 };
+
+// Login Action
+export const loginUserAction = createAsyncThunk(
+  "users/login",
+  async (payload, { rejectWithValue, getState, dispatch }) => {
+    try {
+      // make http request
+      const response = await axios.post(`${baseURL}/users/login`, {
+        email: payload?.email,
+        password: payload?.password,
+      });
+
+      return response.date;
+    } catch (error) {
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
+
+// users slice
+const usersSlice = createSlice({
+  name: "users",
+  initialState,
+  extraReducers: (builder) => {
+    // handleActions
+    // login
+    builder.addCase(loginUserAction.pending, (state, action) => {
+      state.userAuth.loading = true;
+    });
+
+    builder.addCase(loginUserAction.fulfilled, (state, action) => {
+      state.userAuth.userInfo = action.payload;
+      state.userAuth.loading = false;
+    });
+
+    builder.addCase(loginUserAction.rejected, (state, action) => {
+      state.userAuth.error = action.payload;
+      state.userAuth.loading = false;
+    });
+  },
+});
+
+// generate reducer
+
+const usersReducer = usersSlice.reducer;
+
+export default usersReducer;
