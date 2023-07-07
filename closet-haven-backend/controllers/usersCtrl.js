74246159fb40682436672f4447cc762371e90195
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-07-07 23:47:06 +0530

    throwing-error-from-registerUserCtrl-if-user-alrea

--- closet-haven-backend/controllers/usersCtrl.js
+++ closet-haven-backend/controllers/usersCtrl.js
@@ -6,15 +6,13 @@ import asyncHandler from "express-async-handler";
 // @route POST /api/v1/users/register
 // @access Private/Admin
 
-export const registerUserCtrl = async (request, response) => {
+export const registerUserCtrl = asyncHandler(async (request, response) => {
   const { fullname, email, password } = request.body;
   // check user exists
   const userExists = await User.findOne({ email });
   if (userExists) {
     // throw error
-    response.json({
-      msg: "User already exists",
-    });
+    throw new Error("User already exists");
   }
 
   // hash password
@@ -33,7 +31,7 @@ export const registerUserCtrl = async (request, response) => {
     message: "User Registered Successfully",
     data: user,
   });
-};
+});
 
 // @desc Login User
 // @route POST /api/v1/users/login
