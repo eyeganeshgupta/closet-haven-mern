7ea616d53fc4aba923921210f4b1c3ab93884e7a
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-07-08 21:01:57 +0530

    generating-token-for-login-user

--- closet-haven-backend/controllers/usersCtrl.js
+++ closet-haven-backend/controllers/usersCtrl.js
@@ -1,6 +1,7 @@
 import User from "../model/User.js";
 import bcrypt from "bcryptjs";
 import asyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
 
 // @desc Register user
 // @route POST /api/v1/users/register
@@ -49,6 +50,7 @@ export const loginUserCtrl = asyncHandler(async (request, response) => {
       status: "success",
       message: "User LoggedIn Successfully",
       userFound,
+      token: generateToken(userFound?._id),
     });
   } else {
     throw new Error("Invalid Login Credentials");
