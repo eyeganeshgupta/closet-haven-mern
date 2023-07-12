1b3b8435b171d0ed36767905726ff19a04103f6a
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-07-12 10:37:09 +0530

    get-update-delete-categories-controller

--- closet-haven-backend/controllers/categoriesCtrl.js
+++ closet-haven-backend/controllers/categoriesCtrl.js
@@ -1,8 +1,9 @@
+import { response } from "express";
 import Category from "../model/Category.js";
 import asyncHandler from "express-async-handler";
 
 // @desc        Create new category
-// @route       POST /api/v1/categories
+// @route       POST /api/categories
 // @access      Private/Admin
 export const createCategoryCtrl = asyncHandler(async (request, response) => {
   const { name } = request.body;
@@ -23,5 +24,58 @@ export const createCategoryCtrl = asyncHandler(async (request, response) => {
     status: "success",
     message: "Category created successfully",
     category,
+  });
+});
+
+// @desc        Get all categories
+// @route       GET /api/categories
+// @access      Public
+export const getAllCategoriesCtrl = asyncHandler(async (request, response) => {
+  const categories = await Category.find();
+  response.json({
+    status: "success",
+    message: "Categories fetched successfully",
+    categories,
+  });
+});
+
+// @desc        Get single category
+// @route       GET /api/categories/:id
+// @access      Public
+export const getSingleCategoryCtrl = asyncHandler(async (request, response) => {
+  const category = await Category.findById(request.params.id);
+  response.json({
+    status: "success",
+    message: `${category.name} category fetched successfully`,
+    category,
+  });
+});
+
+// @desc        Update category
+// @route       PUT /api/categories/:id
+// @access      Private/Admin
+export const updateCategoryCtrl = asyncHandler(async (request, response) => {
+  const { name } = request.body;
+  // update category
+  const category = await Category.findByIdAndUpdate(
+    request.params.id,
+    { name },
+    { new: true }
+  );
+  response.json({
+    status: "success",
+    message: "Category updated successfully",
+    category,
+  });
+});
+
+// @desc        Delete category
+// @route       DELETE /api/categories/:id
+// @access      Private/Admin
+export const deleteCategoryCtrl = asyncHandler(async (request, response) => {
+  await Category.findByIdAndDelete(request.params.id);
+  response.json({
+    status: "success",
+    message: "Category deleted successfully",
   });
 });
--- closet-haven-backend/routes/categoriesRoute.js
+++ closet-haven-backend/routes/categoriesRoute.js
@@ -1,9 +1,19 @@
 import express from "express";
-import { createCategoryCtrl } from "../controllers/categoriesCtrl.js";
+import {
+  createCategoryCtrl,
+  deleteCategoryCtrl,
+  getAllCategoriesCtrl,
+  getSingleCategoryCtrl,
+  updateCategoryCtrl,
+} from "../controllers/categoriesCtrl.js";
 import { isLoggedIn } from "../middlewares/isLoggedIn.js";
 
 const categoriesRouter = express.Router();
 
 categoriesRouter.post("/", isLoggedIn, createCategoryCtrl);
+categoriesRouter.get("/", getAllCategoriesCtrl);
+categoriesRouter.get("/:id", getSingleCategoryCtrl);
+categoriesRouter.put("/:id", updateCategoryCtrl);
+categoriesRouter.delete("/:id", deleteCategoryCtrl);
 
 export default categoriesRouter;
