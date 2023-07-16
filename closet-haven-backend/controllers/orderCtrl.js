26a1cdff1bdcf7059e2d426064749924b35e34c7
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-07-16 17:24:33 +0530

    order-placement-steps

--- closet-haven-backend/controllers/orderCtrl.js
+++ closet-haven-backend/controllers/orderCtrl.js
@@ -5,7 +5,12 @@ import asyncHandler from "express-async-handler";
 // @route       POST /api/v1/orders
 // @access      Private
 export const createOrderCtrl = asyncHandler(async (request, response) => {
-  response.json({
-    message: "Order controller",
-  });
+  // 1. Find the user
+  // 2. Get the payload(customer, orderItems, shippingAddress, totalPrice)
+  // 3. Check if order is not empty
+  // 4. Place / create order -> `save into dataBase`
+  // 5. Update the product quantity
+  // 6. Make payment (stripe)
+  // 7. Payment webHook
+  // 8. Update the user order
 });
